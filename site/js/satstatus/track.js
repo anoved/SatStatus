7912071ac6bd7a72bd07250876d7e730870bc660
Jim DeVona <jim.devona@gmail.com>
2013-07-05 18:12:56 -0400

    Draft sat track object with draft event-based update mechanism

--- site/js/satstatus/track.js
+++ site/js/satstatus/track.js
@@ -1,15 +1,76 @@
-// tracks should be implemented as objects, so that we can easily display a bunch
-// and just let them all keep track of themselves. updates just provide timestamp
-// to each object, which pushes a point for that timestamp onto its display and
-// pops off the oldest.
 
-init();
+function SatelliteTrace(tle) {
+	if (typeof(tle) === 'undefined') {
+		tle = "1 25544U 98067A   13181.93746528  .00008251  00000-0  14965-3 0   959\n2 25544  51.6493  40.1834 0008920 113.3890  83.5804 15.50498371836833";
+	}
+	this.tle = tle;
+	this.tleLines = this.tle.split("\n");
+	this.satRec = satellite.twoline2satrec(this.tleLines[0], this.tleLines[1]);
+	
+	// populate initial point set
+	this.points = new Array;
+	
+	// this method shifts an old point out and pushes a new one on.
+	this.update = function(date) {
+		
+		// default to update "now"
+		if (typeof(date) === 'undefined') {
+			date = new Date;
+		}
+		
+		// convert timestamp to y mo d h mi s format for propagate()
+		var y = date.getUTCFullYear();
+		var mo = date.getUTCMonth();
+		var d = date.getUTCDate();
+		var h = date.getUTCHours();
+		var mi = date.getUTCMinutes();
+		var s = date.getUTCSeconds();
+		
+		var position_velocity_eci = satellite.propagate(this.satRec, y, mo, d, h, mi, s);
+		
+		// convert inertial to fixed coordinates, based on sidereal time
+		var gmst = satellite.gstime_from_date(y, mo, d, h, mi, s);
+		var position_ecf = satellite.eci_to_ecf(position_velocity_eci[0], gmst);
+		
+		// drop the oldest point and add this one
+		if (this.points.length == 3) {
+			// (don't start dropping points until we've got a couple)
+			this.points.shift();
+		}
+		this.points.push(position_ecf);
+		
+		log(position_ecf);
+		// consider stashing timestamp along with coordinates in "points" array
+		// to facilitate actual path-age visualization (vs relative based on id)
+	}
+	
+	// updateHandler is specifically an event handler, whereas update could be called directly.
+	this.updateHandler = function(event) {
+		// issue is that in an event handler, "this" refers to event, rather than method's parent object...
+		this.update(event.timestamp);
+	}
+	
+	// appending .bind(this) to the event handler function overrides any event-based "this" definition
+	window.addEventListener("updateOrbit", this.updateHandler.bind(this), false);
+}
+
+// when invoked, this function will trigger a timestamped "updateOrbit" event
+// that all SatelliteTrace objects will respond to (by running their update() method)
+function periodicUpdateFunc() {
+	var timestamp = new Date;
+	var timestampedEvent = new CustomEvent("updateOrbit", {"timestamp": timestamp});
+	window.dispatchEvent(timestampedEvent);
+}
+
+//init();
 
 function init() {
 	
 	// to convert arbitrary date or julian date to minutes from epoch:
 	// var j = jday(year, month, day, hour, minute, second);
 	// var m = (j - satrec.jdsatepoch) * minutes_per_day;
+	
+	var tle = "1 25544U 98067A   13181.93746528  .00008251  00000-0  14965-3 0   959\n2 25544  51.6493  40.1834 0008920 113.3890  83.5804 15.50498371836833";
 	
 	var tle1 = "1 25544U 98067A   13181.93746528  .00008251  00000-0  14965-3 0   959";
 	var tle2 = "2 25544  51.6493  40.1834 0008920 113.3890  83.5804 15.50498371836833";
@@ -28,9 +89,9 @@ function init() {
 		log(p_ecf);
 		log(gmst);
 	}
-	
 }
 
+// Append a paragraph containing txt to the log-container div.
 function log(txt) {
 	var d = document.getElementById('log-container');
 	var p = document.createElement('p');
